1f88463557c4974e4209858c22fb2e338cbbae3e
Michael Rea <mwrea@yeet.stagefourbeastmode>
2019-02-06 15:36:27 -0500

    cleaned up directory

--- bigram.sh
+++
@@ -1,6 +0,0 @@
-sed 's/[^a-zA-Z]\+/\n/g' | grep -v '^$' > $$words
-tail -n +2 $$words > $$nextwords
-paste $$words $$nextwords |
-sort | uniq -c
-# remove the temporary files
-rm $$words $$nextwords 

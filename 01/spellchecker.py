e2d028b6be1e333efc3af26743b6b713144d45fd
Michael Rea <mwrea@yeet.stagefourbeastmode>
2019-02-18 15:48:25 -0500

    nearly finished spellchecker

---
+++ 01/spellchecker.py
@@ -0,0 +1,22 @@
+import sys
+
+IN = open('words.hist', 'r')
+dict = []
+for line in IN:
+	word = line.split(' ')[1]
+	print(word)
+	dict.append(word.strip('\n'))
+#print(repr(dict))
+input = sys.stdin.read()
+words = input.split(' ')
+output = []
+
+for w in words:
+	realw = w.strip( '[.,]' )
+	print(realw)
+	if realw not in dict:		
+		w = '*'+w
+	output.append(w)
+
+checked = ' '.join(output)
+print(checked)
